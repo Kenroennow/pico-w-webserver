be9082896c72647c374ff3161ee52c24b62f4474
KenRoennow <ken_roennow@hotmail.com>
2024-12-29 17:04:48 +0100

    IP add shown on serial

--- main.c
+++ main.c
@@ -3,31 +3,43 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "lwipopts.h"
+#include "lwip/ip4_addr.h"
+#include "lwip/netif.h"
 #include "ssi.h"
 #include "cgi.h"
 #include "networkfiles.h"
 
-// WIFI Credentials - take care if pushing to github!
+// WIFI Credentials - take care if pushing to github! add in networkfiles.h
+
 const char WIFI_SSID[] = WIFINAME;
 const char WIFI_PASSWORD[] = WIFIPASSWORD;
-
-//ip_addr_t *ip_address;
 
 int main() {
     stdio_init_all();
 
-    cyw43_arch_init();
+    if (cyw43_arch_init())
+    {
+        printf("Failed to initialize WiFi module\n");
+        return -1;
+    }
 
     cyw43_arch_enable_sta_mode();
 
-    // Connect to the WiFI network - loop until connected
+    // Connect to the WiFi network - loop until connected
     while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) != 0){
         printf("Attempting to connect...\n");
+        sleep_ms(5000); // Wait for 5 seconds before retrying
     }
     // Print a success message once connected
     printf("Connected! \n");
-    
-    // printf("IP address: %s\n", ip4addr_ntoa (ip_address));
+
+    // Get and print the IP address
+    struct netif *netif = netif_list;
+    if (netif_is_up(netif)) {
+        printf("IP address: %s\n", ip4addr_ntoa(netif_ip4_addr(netif)));
+    } else {
+        printf("Failed to get IP address\n");
+    }
 
     // Initialise web server
     httpd_init();
